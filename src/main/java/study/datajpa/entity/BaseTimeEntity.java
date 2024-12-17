1a819d1eccca050578adf522ff7dd1d28857c5f2
sanghui <tkdgml719@gamil.com>
2024-12-17 19:22:16 +0900

    확장 기능 -스프링 데이터 Auditing 적용  -base타입 분리

--- src/main/java/study/datajpa/entity/BaseTimeEntity.java
+++ src/main/java/study/datajpa/entity/BaseTimeEntity.java
@@ -4,21 +4,21 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
-//스프링 데이터 Auditing 적용
+
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @Getter
-public class BaseEntity extends BaseTimeEntity {
+public class BaseTimeEntity {
 
-    //등록자, 수정자
-    @CreatedBy
+    //등록일 , 수정일
+    @CreatedDate
     @Column(updatable = false)
-    private String createBy;
-    @LastModifiedBy
-    private String lastModifiedBy;
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
 }
